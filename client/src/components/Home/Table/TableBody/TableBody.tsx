3203384eda8979be4f12b6489e4559bbf662c375
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-06-30 19:59:13 +0300

    add loader

--- client/src/components/Home/Table/TableBody/TableBody.tsx
+++ client/src/components/Home/Table/TableBody/TableBody.tsx
@@ -1,23 +1,27 @@
 import React, { PropsWithChildren } from 'react'
 import { Item } from '../../../../types/items.types';
+import Loader from '../../../UI/Loader';
 import TableRow from './TableRow/TableRow';
 
 interface TableBodyProps {
-   items: Item[]
+   items: Item[],
+   isLoading: boolean
 }
 
-const TableBody: React.FC<PropsWithChildren<TableBodyProps>> = ({ items }) => {
+const TableBody: React.FC<PropsWithChildren<TableBodyProps>> = ({ items, isLoading }) => {
    return (
       <div className="">
-         {items.length > 0
-            ? items.map((item) => (
-               <TableRow item={item} key={item._id} />
-            ))
-            : <div className='text-center text-lg text-gray-500 py-5'>
-               <p>
-                  Нет данных
-               </p>
-            </div>}
+         {isLoading
+            ? <Loader />
+            : items.length > 0
+               ? items.map((item) => (
+                  <TableRow item={item} key={item._id} />
+               ))
+               : <div className='text-center text-lg text-gray-500 py-5'>
+                  <p>
+                     Нет данных
+                  </p>
+               </div>}
       </div>
    )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
             medium: ['1rem', { lineHeight: '1.125rem' }],
             large: ['1.25rem', { lineHeight: '1.5rem' }],
          },
+
+         keyframes: {
+            list: {
+               '0%': {
+                  'transform': 'scaleY(0)'
+               },
+
+               '100%': {
+                  'transform': 'scaleY(100%)'
+               }
+            },
+         },
+
+         animation: {
+            list: 'list 0.25s ease-in-out',
+         }
       },
    },
    plugins: [],
